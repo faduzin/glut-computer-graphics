92b1ce674c892d883470bc00367eaa46057d4535
Eric Fadul <eric.fadul@gmail.com>
2023-12-10 10:36:52 -0300

    hotfix

--- Semana10/roboteste.c
+++ Semana10/roboteste.c
@@ -1,20 +1,13 @@
 #include <GL/glut.h>
 #include <math.h>
 
-// Função para desenhar um cubo com escalas e normais
 void desenhaCubo(float scaleX, float scaleY, float scaleZ) {
-    // Definindo normais para cada face (perpendiculares às faces)
     float normais[6][3] = {{0, 0, 1}, {0, 0, -1}, {1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}};
-
-    // Definindo vértices do cubo
     float vertices[][3] = {{-0.5, -0.5, -0.5}, {0.5, -0.5, -0.5}, {0.5, 0.5, -0.5}, {-0.5, 0.5, -0.5},
                            {-0.5, -0.5, 0.5}, {0.5, -0.5, 0.5}, {0.5, 0.5, 0.5}, {-0.5, 0.5, 0.5}};
-
-    // Definindo faces do cubo
     int faces[][4] = {{0, 1, 2, 3}, {4, 5, 6, 7}, {0, 3, 7, 4},
                       {1, 2, 6, 5}, {0, 1, 5, 4}, {2, 3, 7, 6}};
 
-    // Desenhando o cubo
     glBegin(GL_QUADS);
     for (int i = 0; i < 6; i++) {
         glNormal3fv(normais[i]);
@@ -27,54 +20,45 @@ void desenhaCubo(float scaleX, float scaleY, float scaleZ) {
     glEnd();
 }
 
-// Função para desenhar a cabeça do robô
 void desenhaCabeca() {
     glColor3f(1.0, 0.0, 0.0);
     desenhaCubo(0.8, 0.8, 0.8);
 }
 
-// Função para desenhar o torso do robô
 void desenhaTorso() {
     glColor3f(0.0, 1.0, 0.0);
     desenhaCubo(1.0, 1.5, 0.6);
 }
 
-// Função para desenhar um braço do robô
 void desenhaBraco() {
     glColor3f(0.0, 0.0, 1.0);
     desenhaCubo(0.4, 1.2, 0.4);
 }
 
-// Função para desenhar o antebraço do robô
 void desenhaAntebraco() {
     glColor3f(1.0, 1.0, 0.0);
     desenhaCubo(0.4, 1.0, 0.4);
 }
 
-// Função para desenhar uma perna do robô
 void desenhaPerna() {
     glColor3f(0.0, 1.0, 1.0);
     desenhaCubo(0.6, 1.5, 0.6);
 }
 
-// Função para desenhar o joelho do robô
 void desenhaJoelho() {
     glColor3f(1.0, 0.0, 1.0);
     desenhaCubo(0.6, 0.6, 0.6);
 }
 
-// Função para desenhar a panturrilha do robô
 void desenhaPanturrilha() {
     glColor3f(0.5, 0.5, 0.5);
     desenhaCubo(0.6, 1.2, 0.6);
 }
 
-// Função de desenho principal
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    // Desenhando as partes do robô
     desenhaCabeca();
     desenhaTorso();
 
@@ -113,7 +97,6 @@ void display() {
     glutSwapBuffers();
 }
 
-// Função principal
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
